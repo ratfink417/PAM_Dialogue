7cf7a8683e86adbb8ea18954081ffb40840fa72a
John Stevens <ratfink417@gmail.com>
2016-03-31 12:57:37 -0700

    Update newpam.c

--- newpam.c
+++ newpam.c
@@ -7,5 +7,6 @@ PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const ch
 
 /* customized portion*/
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
-return pam_dailog(flags,pamh,1,"how many letters in the word derp: ","derp");
+int retval = pam_dailog(flags,pamh,1,"how many letters in the word derp: ","derp");
+return retval;
 }
